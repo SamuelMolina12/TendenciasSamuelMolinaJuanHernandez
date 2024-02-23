5262eb9a7cc04a69f781dba3711f5c97ce6c2d6e
Jcamilohz <juancaher2011@gmail.com>
2024-02-23 17:05:34 -0500

    CGH HospitalCode

--- HospitalCode/Menus/AdminMenu.py
+++ HospitalCode/Menus/AdminMenu.py
@@ -19,30 +19,80 @@ def adminMenu(hospital,user):
 
 def ManageAdmin(hospital):
     while True:
-        option=input("1.Crear administrador \n2. Editar administrador\n3. Eliminar administrador \n4. Cancelar\n" )
+        option=input("1.Crear administrador \n2.Editar administrador\n3.Eliminar administrador \n4.Mostrar administradores \n5.Cancelar\n")
         if option=="1":
-           createAdmin(hospital)
+           createUser(hospital,"admin")
+        if option=="2":
+           pass
+        if option == "3":
+           DeleteUser(hospital, id)
+        if option == "4":
+          showUser(hospital,"admin")
+        if option == "5":
+          
+            return           
+        
+            
+def ManageDoctor(hospital):
+     while True:
+        option=input("1.Crear doctor \n2.Editar doctor\n3.Eliminar doctor \n4.Mostrar doctores \n5.Cancelar\n")
+        if option=="1":
+            createUser(hospital,"doctor")
         if option=="2":
          print("2")
         if option == "3":
-           print("3")
-      
+           DeleteUser(hospital, id)
         if option == "4":
-            print("cerrando sesion")
-            return           
+          showUser(hospital,"doctor")
+        if option == "5":
+          
+            return  
         
-    
-    
-def ManageDoctor(hospital):
-    pass
-def ManageNurse(hospital):
-    pass
-def ManageSupport(hospiatal):
-    pass
 
-def createAdmin(hospital):
+def ManageNurse(hospital):
+     while True:
+        option=input("1.Crear enfermera \n2.Editar enfermera \n3.Eliminar enfermera \n4.Mostrar enfermera \n5.Cancelar\n")
+        if option=="1":
+            createUser(hospital,"enfermera")
+        if option=="2":
+         print("2")
+        if option == "3":
+           DeleteUser(hospital, id)
+        if option == "4":
+          showUser(hospital,"enfermera")
+        if option == "5":
+          
+            return  
+        
+def ManageSupport(hospital):
+     while True:
+        option=input("1.Crear soporte \n2.Editar soporte \n3.Eliminar soporte \n4.Mostrar soporte \n5.Cancelar\n")
+        if option=="1":
+            createUser(hospital,"soporte")
+        if option=="2":
+         print("2")
+        if option == "3":
+           DeleteUser(hospital, id)
+        if option == "4":
+          showUser(hospital,"soporte")
+        if option == "5":
+          
+            return  
+        
+
+def createUser(hospital,role):
     try:
-        personTypeValidator.createAdmin(hospital,"administrador")
-        print("se ha creado el administrador")
+        personTypeValidator.createUser(hospital,role)
+        print("se ha creado el " + role)
     except Exception as error:
         print(str(error))
+
+def showUser(hospital,role):
+    try:
+        personTypeValidator.ShowUsers(hospital,role)
+    except Exception as error:
+        print(str(error))
+
+def DeleteUser(hospital, id):
+    id = input("Ingrese la identificación del usuario que desea eliminar: ")
+    personTypeValidator.DeleteUser(hospital,id)
--- HospitalCode/Validators/personTypeValidator.py
+++ HospitalCode/Validators/personTypeValidator.py
@@ -1,25 +1,51 @@
 from Validators.typeValidator import *
 import Service.AdminService as adminService
 
-
-def createAdmin(hospital,role):
-    name=input("ingrese nombre del " + role)
-    TextValidator(name,"nombre de " + role)
-    id=NumberValidator(input("ingrese la cedula de " + role), "cedula de " + role)
-    username=input("ingrese usuario del " + role)
-    TextValidator(name,"usuario de " + role) 
-    genre=input("ingrese el genero" + role)
-    TextValidator(name, "genero de " +role)
-    mail=input("ingrese el correo" + role)
-    TextValidator(name, "correo de " +role)
-    telephone=input("ingrese el telefono" + role)
-    TextValidator(name, "telefono  de " +role)
-    birth=NumberValidator(input("ingrese la fecha de nacimiento" + role),"fecha de nacimiento de" + role)
-    address=input("ingrese la direccion" + role)
-    TextValidator(name, "direccion de  " +role)
-    role=input("ingrese el genero" + role)
-    TextValidator(name, "genero de " +role)
-    password=input("ingrese contraseña del " + role)
-    PasswordValidator(name,"contraseña de " + role)
-    adminService.createAdmin(hospital,name,id,genre,mail,telephone,birth,address,role,username,password)
+def createUser(hospital,role):
+    print("Ingreso a la creacion del rol " + role)
+    name=input("ingrese el nombre " + "\n")
+    TextValidator(name,"nombre de " + role  + "\n")
+    id=NumberValidator(input("ingrese la cedula  " + "\n"), "cedula de " + role )
+    username=input("ingrese el usuario  " +  "\n")
+    TextValidator(name,"usuario de " + role  + "\n" ) 
+    genre=input("ingrese el genero " +  "\n")
+    TextValidator(name, "genero de " +role  + "\n")
+    mail=input("ingrese el correo " +  "\n")
+    TextValidator(name, "correo de " +role  + "\n")
+    telephone=input("ingrese el numero tlefonico "   + "\n")
+    TextValidator(name, "telefono  de " +role  + "\n")
+    birth=NumberValidator(input("ingrese la fecha de nacimiento " + "\n"),"fecha de nacimiento de" + role  )
+    address=input("ingrese la direccion " +  "\n")
+    TextValidator(name, "direccion de  " +role  + "\n")
+    role=role
+    TextValidator(name, "genero de " +role  + "\n")
+    password=input("Ingrese la contraseña  " + "\n")
+    PasswordValidator(password, "contraseña de " + role + "\n")
+    adminService.createAdmin(hospital, name, id, genre, mail, telephone, birth, address, role, username, password)
     
+def ShowUsers(hospital,role):
+    allUsers = hospital.persons
+    for user in allUsers:
+        if user.role == role:
+            print(f"Nombre: {user.name}")
+            print(f"Cedula: {user.id}")
+            print(f"genero: {user.genre}")
+            print(f"Email: {user.mail}")
+            print(f"Telefono: {user.telephone}")
+            print(f"Fecha de nacimiento: {user.birth}")
+            print(f"Direccion: {user.address}")
+            print(f"Rol: {user.role}")
+            print(f"Usuario: {user.username}")
+            print(f"")
+
+           
+def DeleteUser(hospital, id):
+    id = int(id)  
+    if adminService.DeleteUser(hospital, id):
+        for i, user in enumerate(hospital.persons):
+            if user.id == id:
+                del hospital.persons[i]
+                print("Usuario eliminado exitosamente.")
+                return
+    else:
+        print("No se encontró ningún usuario con esa identificación.")
