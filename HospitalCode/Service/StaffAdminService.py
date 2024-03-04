b7ce5687d26e8d5e57a9319820881694fe764156
SamuelMolina12 <sm34066@gmail.com>
2024-03-04 17:09:23 -0500

    CHG

--- HospitalCode/Service/StaffAdminService.py
+++ HospitalCode/Service/StaffAdminService.py
@@ -7,15 +7,17 @@ def validateId(hospital,id):
     return None
 
 
-def createPatient(hospital,id,name,genre,mail,telephone,birth,address,emergencyContact,policy,clinicHistory):
-    patient=validateId(hospital,id)
-    print("busca paciente")
+def createPatient(hospital, id, name, genre, mail, telephone, birth, address, emergency_contact_info, policy_info):
+    patient = validateId(hospital, id)
     if patient:
-        raise Exception("ya existe una persona con esa cedula registrada")
-    patient=models.Patient(id,name,genre,mail,telephone,birth,address,emergencyContact,policy,clinicHistory)
-    
+        raise Exception("Ya existe una persona con esa cédula registrada")
+    patient = models.Patient(id, name, genre, mail, telephone, birth, address)
+    emergency_contact = models.EmergencyContact(id, emergency_contact_info['name'], emergency_contact_info['relationship'], emergency_contact_info['telephone'])
+    patient.emergencyContact = emergency_contact
+    policy = models.Policy(id, policy_info['insuranceCompany'], policy_info['policynumber'], policy_info['statePolicy'], policy_info['termPolicy'])
+    patient.policy = policy
     hospital.patient.append(patient)
-   
+
     return patient
 
 def findPatient(hospital, id):
@@ -24,3 +26,16 @@ def findPatient(hospital, id):
         if human.id == id:
             return human
     return None
+
+def deletePatient(hospital,id):
+    for user in hospital.patient:
+        if user.id == id:
+            return True
+    return False
+
+def updateUser(hospital,id):
+    for user in hospital.patient:
+        if user.id == int(id):
+            return user
+    return None
+
